3bf70c2ceec0205b1c4b9f0c9e0ccdfb3c696d9e
mbn217 <nherimohamed@gmail.com>
2018-07-24 20:25:26 -0400

    new changes

---
+++ JavaMasterTB/src/Section10/DeskPhone.java
@@ -0,0 +1,38 @@
+package Section10;
+
+public class DeskPhone  implements ITelephone{
+	
+	private int myNumber;
+	private boolean isRinging;
+
+	@Override
+	public void powerOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dial(int phoneNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void answer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean callPhone(int phneNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRinging() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
